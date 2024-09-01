38842c8d60975dd667510ddc0706006d31c68a50
Lleewss <johnlewiselopre@gmail.com>
2024-09-01 14:35:19 +0800

    updated

---
+++ wp-content/themes/generatepress/inc/customizer/controls/class-color-control.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Class &quot;WP_Customize_Color_Control&quot; not found in /Users/johnlewiselopre/Local Sites/kiddiefashionistas/app/public/wp-content/themes/generatepress/inc/customizer/controls/class-color-control.php:15
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/johnlewiselopre/Local Sites/kiddiefashionistas/app/public/wp-content/themes/generatepress/inc/customizer/controls/class-color-control.php</b> on line <b>15</b><br>
